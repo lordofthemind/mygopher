8693d6e5e172ec6669ef7612fd2d948e1ffb2e1d
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-09-28 17:04:10 +0000

    new version

--- gophersmtp/version.go
+++ gophersmtp/version.go
@@ -1,7 +1,7 @@
 package gophersmtp
 
 // Version of the gophersmtp package
-const Version = "1.0.2"
+const Version = "1.0.1"
 
 // Author of the gophersmtp package
 const Author = "github.com/lordofthemind"
