cc0d1182f76f0bb74a44d08810eb141364992899
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-09-29 17:47:13 +0000

    gin update

--- gophergin/SetUpGinServer.go
+++ gophergin/SetUpGinServer.go
@@ -18,22 +18,18 @@ import (
 //
 // Fields:
 // - Port: Port number to run the server on.
-// - StaticPath: Path to serve static files from.
-// - TemplatePath: Path to HTML templates for rendering.
 // - UseTLS: Enable TLS (HTTPS) if true.
 // - TLSCertFile: Path to the TLS certificate file (required if UseTLS is true).
 // - TLSKeyFile: Path to the TLS key file (required if UseTLS is true).
 // - UseCORS: Enable CORS (Cross-Origin Resource Sharing) if true.
 // - CORSConfig: Configures allowed origins, headers, and methods for CORS.
 type ServerConfig struct {
-	Port         int
-	StaticPath   string
-	TemplatePath string
-	UseTLS       bool
-	TLSCertFile  string
-	TLSKeyFile   string
-	UseCORS      bool
-	CORSConfig   cors.Config
+	Port        int
+	UseTLS      bool
+	TLSCertFile string
+	TLSKeyFile  string
+	UseCORS     bool
+	CORSConfig  cors.Config
 }
 
 // Server interface defines the behavior of a Gin server.
@@ -51,7 +47,7 @@ type Server interface {
 // ServerSetup defines the behavior for setting up a Gin server.
 //
 // Methods:
-// - SetUpRouter: Configures and returns a new Gin engine with static file and template paths.
+// - SetUpRouter: Configures and returns a new Gin engine.
 // - SetUpTLS: Configures TLS settings if required (returns a tls.Config instance).
 // - SetUpCORS: Applies CORS middleware to the Gin engine if enabled.
 type ServerSetup interface {
@@ -63,22 +59,15 @@ type ServerSetup interface {
 // ServerSetupImpl is the concrete implementation of ServerSetup.
 type ServerSetupImpl struct{}
 
-// SetUpRouter sets up a Gin server with static file and template paths.
+// SetUpRouter sets up a Gin server.
 //
 // Parameters:
-// - config: The server configuration for static files and template paths.
+// - config: The server configuration.
 //
 // Returns:
 // - *gin.Engine: A configured Gin engine.
 func (s *ServerSetupImpl) SetUpRouter(config ServerConfig) *gin.Engine {
 	router := gin.Default()
-
-	// Serve static files from the configured path.
-	router.Static("/static", config.StaticPath)
-
-	// Load HTML templates from the configured path.
-	router.LoadHTMLGlob(config.TemplatePath)
-
 	return router
 }
 
@@ -219,8 +208,6 @@ func (gs *GinServer) GracefulShutdown() {
 //	func main() {
 //	    config := gophergin.ServerConfig{
 //	        Port:         8080,
-//	        StaticPath:   "./static",
-//	        TemplatePath: "./templates/*.html",
 //	        UseTLS:       true,
 //	        TLSCertFile:  "./certs/server.crt",
 //	        TLSKeyFile:   "./certs/server.key",
